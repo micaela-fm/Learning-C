0fef06f290a9e8243e860971cded1c282cc59bc0
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-08-02 00:50:09 +0100

    Add project files.

---
+++ PrintMessage/PrintMessage/main.c
@@ -0,0 +1,18 @@
+/*
+*  File: main.c
+*  Author: Micaela Macatrao
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Prints a message
+*/
+int main(int argc, char** argv)
+{
+	// print message 
+	// printf must have arguments
+	printf("Hi, n00b!\n");
+	return (EXIT_SUCCESS); 
+}
