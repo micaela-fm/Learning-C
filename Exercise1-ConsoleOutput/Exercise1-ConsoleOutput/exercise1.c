5a1d2c929f521943004604b63a9490cc3be84bfe
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-08-02 00:58:43 +0100

    Exercise 1

---
+++ Exercise1-ConsoleOutput/Exercise1-ConsoleOutput/exercise1.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: Micaela FM
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*
+  * Prints names and some other information
+  */
+int main(int argc, char** argv)
+{
+	printf("My name is Micaela \n"); 
+	printf("My pet's name is Valkiria \n"); 
+	printf("I don't really like video games \n"); 
+	return (EXIT_SUCCESS);
+}
