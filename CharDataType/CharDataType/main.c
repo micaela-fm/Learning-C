86043cd04e9a046d1b57bf233ee2d5ef78d63c10
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-08-02 23:35:40 +0100

    Char data type

---
+++ CharDataType/CharDataType/main.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: Micaela FM
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*
+  * Demonstrates char data type
+  */
+int main(int argc, char** argv)
+{
+	char menuChoice = 'z'; // char can be assigned a character or an int with the corresponding ASCII code
+// 	menuChoice = 120; // menuChoice = 'x'
+
+	return (EXIT_SUCCESS);
+}
