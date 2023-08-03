2fa5b43792fc8477d62e0c1eedfd57cac3cff654
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-08-03 23:04:38 +0100

    Exercise 6: Energetic Einstein

---
+++ Exercise6-EnergeticEinstein/Exercise6-EnergeticEinstein/main.c
@@ -0,0 +1,40 @@
+/*
+ * File:   main.c
+ * Author: Micaela FM
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define SPEED_OF_LIGHT 299792458
+
+ /*
+  * Objective: Implement a scientific computation 
+  * and reflect on the implications of the result
+  */
+int main(int argc, char** argv)
+{
+
+// Problem 1 – Declare constant and variables
+	float mass; 
+	float energy; 
+	
+	// Print for and scan mass
+	printf("Enter mass (kg): "); 
+	scanf("%f", &mass); 
+	printf("\n"); 
+
+	// Print energy
+	energy = mass * powf((float)SPEED_OF_LIGHT, 2); 
+	printf("Energy = %f", energy); 
+
+// Problem 2 – Get mass from user and calculate energy
+
+// Problem 3 – Think about what it all means
+
+	printf("\n");
+	return (EXIT_SUCCESS);
+}
